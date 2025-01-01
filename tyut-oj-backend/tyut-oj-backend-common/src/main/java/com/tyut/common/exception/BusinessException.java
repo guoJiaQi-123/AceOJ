ea87939d9f75682175aca1d8863d8835ac307f8a
GuoJiaqi <guojiaqi5946@outlook.com>
2025-01-01 17:20:41 +0800

    微服务改造完结，扩展完结

--- tyut-oj-backend/tyut-oj-backend-common/src/main/java/com/tyut/common/exception/BusinessException.java
+++ tyut-oj-backend/tyut-oj-backend-common/src/main/java/com/tyut/common/exception/BusinessException.java
@@ -6,8 +6,8 @@ import com.tyut.common.common.ErrorCode;
 /**
  * 自定义异常类
  *
- * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
- * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ * @author 
+ * @from 
  */
 public class BusinessException extends RuntimeException {
 
--- tyut-oj-backend/tyut-oj-backend-gateway/src/main/java/com/tyut/gateway/OjBackendGatewayApplication.java
+++ tyut-oj-backend/tyut-oj-backend-gateway/src/main/java/com/tyut/gateway/OjBackendGatewayApplication.java
@@ -3,15 +3,10 @@ package com.tyut.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
-@EnableScheduling
-@EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
-@ComponentScan("com.tyut")
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableDiscoveryClient
 public class OjBackendGatewayApplication {
 
